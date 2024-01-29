3259583531633757066eb741d0c2dce56dc16848
ash777w <126274679+ash777w@users.noreply.github.com>
2024-01-29 18:17:28 +0500

    Update gp-tagall.js

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetad
 handler.help = ['tagall']
 handler.tags = ['group']
 handler.command = ['tagall']
-handler.admin = true
+handler.Premium = true
 handler.group = true
 
 export default handler
